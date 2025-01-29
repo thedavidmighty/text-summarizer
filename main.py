b2ed093104db627e3c49311c02bf9b4e535cf4e1
thedavidmighty <thedavidukwu@gmail.com>
2025-01-29 22:02:38 +0000

    29jan2025

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from text_summarizer.logging import logger
 
 stage_name = "Data Ingestion Stage"
@@ -7,6 +8,17 @@ try:
     logger.info(f"Stage {stage_name} has been initiated")
     data_ingestion_training_pipeline = DataIngestionTrainingPipeline()
     data_ingestion_training_pipeline.main()
+    logger.info(f"Stage {stage_name} has run successfully")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+stage_name = "Data Validation Stage"
+
+try:
+    logger.info(f"Stage {stage_name} has been initiated")
+    data_validation_training_pipeline = DataValidationTrainingPipeline()
+    data_validation_training_pipeline.main()
     logger.info(f"Stage {stage_name} has run successfully")
 except Exception as e:
     logger.exception(e)
