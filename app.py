366f605f2c554f11551eb7c65143f85dbea49562
thedavidmighty <thedavidukwu@gmail.com>
2025-02-18 12:30:41 +0000

    User interface on Fast API

---
+++ app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+import uvicorn
+import sys
+import os
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from text_summarizer.pipeline.prediction import PredictionPipeline
+
+text:str = "What is Text Summarisation?"
+
+app = FastAPI()
+
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.get("/train")
+async def training():
+    try:
+        os.system("python main.py")
+        return Response("Training completed successfully !!")
+    
+    except Exception as e:
+        return Response(f"An Error Occurred! {e}")
+
+@app.post("/predict")
+async def predict_route(text):
+    try:
+        obj = PredictionPipeline()
+        text = obj.predict(text)
+        return Response(text)
+    
+    except Exception as e:
+        raise e
+    
+if __name__ == "__main__":
+    uvicorn.run(app, host = "0.0.0.0", port = 8080)
